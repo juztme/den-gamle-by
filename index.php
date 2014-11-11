c527823691ae060bf92a1c1dd8a5886526a93ce4
Marina <juztmejuztme@yahoo.com>
2014-11-11 12:43:32 +0100

    init

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Christmas at Den Gamle By</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<link href='http://fonts.googleapis.com/css?family=Share+Tech' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<!--first page-->
+<div id="first">
+<div id="logo1"><a href="http://www.dengamleby.dk/the-old-town/"><img src="dengamleby-logo.gif" alt="dengamleby" width="140" height="60"/></a></div>
+<div id="logo2"><a href="http://www.dobivida.wordpress.com/"><img src="logo_icon.png" alt="logo" width="140" height="60"></a></div>
+<div id="logofb"><a href="https://www.facebook.com/DenGamleBy"><img src="fb_icon.png"  alt="facebook" width="60" height="60"/></a></div>
+<div id="logoyt"><a href="http://www.youtube.com/user/dengamlebymuseum"><img src="yt_icon.png" alt="youtube" width="59" height="59"/></a></div>
+</div>
+<!--end of first page-->
+
+<!--2nd page-->
+<div id="second">
+<div id="text1">
+<p>Explore the historical shops and discover unique presents, including a bookstore, a baker and a merchant. In 1927, the Ironmonger tempts children and adults with mechanical toys and all little boys dream of a steam engine for Christmas.</p>
+</div>
+
+<div class="image"><img src="gb04.jpg" alt="table" height="400" width="300"/></div>
+
+<div id="text2">
+<p>Experience how Danes have celebrated the holidays for centuries. Exciting activities await the entire family with historical shops, Living History, traditional Danish treats, Christmas market and entertainment in the weekends.</p></div>
+
+<div id="text3"><p>Living history in the Merchant’s House, 1864, where visitors may help the kitchen maids preparing for Christmas. You can also travel in style in the traditional Danish post carriage, or be tempted by the many delicacies at the Christmas market.</p></div>
+
+<div id="text4"><p>Traditional Christmas table from 1723. Throughout the museum, visitors get a glimpse of how the Danish Christmas has developed from the 1600s until 1974.</p></div>
+
+</div>
+<!--end of 2nd page-->
+
+<!--3rd page-->
+<div id="third">
+<div id="envelope"><img src="logo.gif" alt="our logo" width="433" height="456"/></div>
+<div id="competition"><p>COMPETITION</p></div>
+<div id="oldschool"><p>How Oldschool Danish Can You Get?</p></div>
+<div id="paragraph"><p>Christmas is soon upon us and it’s time to get creative! 
+That is why Den Gamle By in Aarhus would like to make something exciting to happen. Two competitions for everyone who would like to compete. The first competition is a Photo competition and the second one is a Video competition. They are both explained in the pages below.<br/> 
+There will be lights and Christmas Decorations in and outside the houses, and different events will take place from 16th November to the 30th of December.<br/> Read more about the events here:<a href="http://www.dengamleby.dk/christmas/"> Christmas in Den Gamle By</a>.<br/>
+And remember, with a student card you will benefit of a discount. 
+</p></div>
+<div id="signup">SIGN UP</div>
+
+</div>
+<!--end of 3rd page-->
+
+<!--4th page-->
+<div id="fourth">
+<div id="photocomp"><p>PHOTO COMPETITION</p></div>
+<div id="phototext"><p>We Want YOU!!! 
+This competition is for you, the one who can make the best picture of the Danish Christmas in the old Days! Den Gamle By’s Event will take place from the 16th of November to the 30th of December. The houses you will see there are from the last centuries and will all be decorated and light up, so come down and take a look and make some awesome pictures that you afterwards can upload to our website, after signing up for the competition.
+Of course, we also have very cool prizes for the winner! You can win a brand new EOS 700D Camera from Canon. The winner will be contacted on the 18th of January!  
+</p></div>
+
+<div id="imgslider"><img src="slider.png" alt="Christmas at Den Gamle By" width="460" height="337"/></div>
+</div>
+<!--end of 4th page-->
+
+<!--5th page-->
+<div id="fifth">
+<!--<div id= "video"><iframe width="560px" height="315px" src="http://www.youtube.com/embed/la-bKJdS8JQ" frameborder="0" allowfullscreen></iframe></div>-->
+<div id="videocomp"><p>VIDEO COMPETITION</p></div>
+<div id="videotext"><p>Find your inner Spielberg and come down to Den Gamle By in Aarhus! From the 16th of November to the 30th of December, Den Gamle By will be filled with Christmas decorations and engulfed by the Christmas spirit and you could find lots of traditional food from the old days in Denmark. Come down and make a video of this Christmas event and join the competition!
+Afterwards you have to sign up here, upload your video and send it to us. On the 18th of January the winner will be contacted. The Prize is a Legria HF R48 Camera from Canon.</p></div>
+<div id= "video"><iframe width="560" height="315" src="http://www.youtube.com/embed/jqof6gVlOyY" allowfullscreen></iframe></div>
+
+</div>
+<!--end of 5th page-->
+
+<!--6th page-->
+<div id="sixth">
+<div id="houses"><img src="houses.png" alt="competition logo" width="420" height="150"/></div>
+
+<div id="contact">
+ <form action="handle_form.php" method="post">
+          <fieldset>
+              <legend> Have questions? Write to us! </legend>
+              <br/>
+              <label for="name">Name:</label>
+              
+              <input type="text" name="name" id="name">
+      
+              <label for="email">Email:</label>
+              <input type="text" name="email" id="email">
+              <br/>
+              <br/>
+              
+              <div id="subjectmessage"><label for="subject">Subject:</label>
+              <br/>
+              <input type="text" name="subject" id="subject"></div> 
+              <br/>
+              <label for="message">Message:</label>
+              <br/><textarea cols="60" rows="5" name="message" id="message"></textarea>
+              <input type="submit" name="submit" value="Send my feedback" id="sendbutton" >
+          </fieldset>
+          </form>    
+</div>
+
+<div id="snowglobe"><img src="snowglobe.gif" alt="awesome snowglobe" width="400" height="400"/></div>
+
+</div>
+
+<!--6th page end-->
+</body>
+</html>
